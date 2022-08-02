d89d0e47333b78c32f78286fefbd7ca6e16a5f34
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2022-08-02 17:55:10 +0100

    test: add unit tests

--- handler/processors_test.go
+++ handler/processors_test.go
@@ -17,6 +17,97 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func buildPayload(payload []byte) *json.RawMessage {
+	rawPayload := json.RawMessage(payload)
+	return &rawPayload
+}
+
+func TestParseEvent_Failure(t *testing.T) {
+	event := `{"type": "ping",}`
+	gotEvent, err := handler.ParseEvent(event)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, gotEvent)
+}
+
+func TestParseEvent(t *testing.T) {
+	event := `{"action": "ping"}`
+	wantEvent := &handler.ActionEvent{
+		ActionName: github.String("ping"),
+	}
+
+	gotEvent, err := handler.ParseEvent(event)
+
+	assert.Nil(t, err)
+	assert.Equal(t, wantEvent, gotEvent)
+}
+
+func TestProcessEvent_Failure(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	owner := "reviewpad"
+	repo := "reviewpad"
+	httpmock.RegisterResponder("GET", fmt.Sprintf("https://api.github.com/repos/%v/%v/pulls", owner, repo),
+		func(req *http.Request) (*http.Response, error) {
+			return nil, fmt.Errorf("error")
+		},
+	)
+
+	tests := map[string]struct {
+		event *handler.ActionEvent
+	}{
+		"pull_request": {
+			event: &handler.ActionEvent{
+				EventName:    github.String("pull_request"),
+				EventPayload: buildPayload([]byte(`{,}`)),
+			},
+		},
+		"unsupported_event": {
+			event: &handler.ActionEvent{
+				EventName: github.String("branch_protection_rule"),
+				EventPayload: buildPayload([]byte(`{
+					"action": "branch_protection_rule"
+				}`)),
+			},
+		},
+		"cron": {
+			event: &handler.ActionEvent{
+				EventName:  github.String("schedule"),
+				Token:      github.String("test-token"),
+				Repository: github.String("reviewpad/reviewpad"),
+			},
+		},
+		"workflow_run_match": {
+			event: &handler.ActionEvent{
+				EventName: github.String("workflow_run"),
+				Token:     github.String("test-token"),
+				EventPayload: buildPayload([]byte(`{
+					"action": "completed",
+					"repository": {
+						"name": "reviewpad",
+						"owner": {
+							"login": "reviewpad"
+						}
+					},
+					"workflow_run": {
+						"head_sha": "4bf24cc72f3a62423927a0ac8d70febad7c78e0g"
+					}
+				}`)),
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			gotVal, gotErr := handler.ProcessEvent(test.event)
+
+			assert.Nil(t, gotVal)
+			assert.NotNil(t, gotErr)
+		})
+	}
+}
+
 func TestProcessEvent(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
@@ -48,11 +139,6 @@ func TestProcessEvent(t *testing.T) {
 			return resp, nil
 		},
 	)
-
-	buildPayload := func(payload []byte) *json.RawMessage {
-		rawPayload := json.RawMessage(payload)
-		return &rawPayload
-	}
 
 	tests := map[string]struct {
 		event   *handler.ActionEvent
@@ -87,6 +173,29 @@ func TestProcessEvent(t *testing.T) {
 				}`)),
 			},
 			wantVal: []int{130},
+		},
+		"pull_request_review": {
+			event: &handler.ActionEvent{
+				EventName: github.String("pull_request_review"),
+				Token:     github.String("test-token"),
+				EventPayload: buildPayload([]byte(`{
+					"action": "opened",
+					"number": 130,
+					"pull_request": {
+						"body": "## Description",
+						"number": 130
+					}
+				}`)),
+			},
+			wantVal: []int{130},
+		},
+		"cron": {
+			event: &handler.ActionEvent{
+				EventName:  github.String("schedule"),
+				Token:      github.String("test-token"),
+				Repository: github.String("reviewpad/reviewpad"),
+			},
+			wantVal: []int{130, aladino.DefaultMockPrNum},
 		},
 		"workflow_run_match": {
 			event: &handler.ActionEvent{
